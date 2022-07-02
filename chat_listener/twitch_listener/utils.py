c15ea8e441fc6272769c0fb592c7782ecf7b7b66
Anders <asimmeth@gmail.com>
2022-07-02 18:51:45 +0000

    chat listener upgrades

---
+++ chat_listener/twitch_listener/utils.py
@@ -0,0 +1,24 @@
+import logging
+import requests
+import json
+
+def setup_loggers(name, log_file, level=logging.INFO):
+        formatter = logging.Formatter('%(asctime)s — %(message)s')
+        handler = logging.FileHandler(log_file)        
+        handler.setFormatter(formatter)
+    
+        logger = logging.getLogger(name)
+        logger.setLevel(level)
+        logger.addHandler(handler)
+
+        return logger
+    
+def is_live(channel_list):
+    
+    for channel in channel_list:
+        contents = requests.get('https://www.twitch.tv/' +channel).content.decode('utf-8')
+
+        if 'isLiveBroadcast' not in contents:
+            channel_list.remove(channel)
+        
+    return channel_list
