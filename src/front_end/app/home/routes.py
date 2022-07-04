f462f56426725a75f58a01d2ffd6e0f8fce9bd05
vbeohar <vbeohar@berkeley.edu>
2022-07-04 04:39:51 -0400

    Added frontend AJAX jQuery with Flask

--- src/front_end/app/home/routes.py
+++ src/front_end/app/home/routes.py
@@ -4,10 +4,11 @@ Copyright (c) 2019 - present AppSeed.us
 """
 
 from app.home import blueprint
-from flask import render_template, redirect, url_for, request
+from flask import render_template, redirect, url_for, request, jsonify
 from flask_login import login_required, current_user
 from app import login_manager
 from jinja2 import TemplateNotFound
+import random
 
 @blueprint.route('/index')
 @login_required
@@ -50,3 +51,55 @@ def get_segment( request ):
 
     except:
         return None  
+
+@blueprint.route('/test')
+@login_required
+def testfn():
+    # GET request
+    if request.method == 'GET':
+        message = {'greeting':'Hello from Flask!!!'}
+        return jsonify(message)  # serialize and use JSON headers
+        # return render_template('index.html', msg=jsonify(message))
+    # POST request
+    if request.method == 'POST':
+        # print(request.get_json())  # parse as JSON
+        return 'Sucesss', 200
+        # return render_template('index.html', msg=jsonify(message))
+
+
+@blueprint.route('/recieve_streamer_visitors')
+@login_required
+def get_id():
+   # the_id = request.args.get('button_id')
+   streamer_choose_id = request.args.get('streamer_choose_id')
+   print(streamer_choose_id)
+   if streamer_choose_id == 'xQcOW':
+    return "<p> 1,334,452 </p>"
+   elif streamer_choose_id == 'Summit1G':
+    return "<p> 2,345,453 </p>"
+   elif streamer_choose_id == 'Shroud':
+    return "<p> 685,765 </p>" 
+   
+   #return "<p> Select a stream </p>" 
+
+
+@blueprint.route('/refresh_sentiments')
+@login_required
+def get_sentiments():
+   num = random.uniform(0, 1)
+   print('Hello from refresh_sentiments: ', num)
+   return str(num)
+    #    streamer_choose_id = request.args.get('streamer_choose_id')
+    #    print(streamer_choose_id)
+    #    if streamer_choose_id == 'xQcOW':
+    #     return "<p> 1,334,452 </p>"
+    #    elif streamer_choose_id == 'Summit1G':
+    #     return "<p> 2,345,453 </p>"
+    #    elif streamer_choose_id == 'Shroud':
+    #     return "<p> 685,765 </p>"    
+
+
+@blueprint.route('/index')
+@login_required
+def home():
+   return render_template('index.html')
