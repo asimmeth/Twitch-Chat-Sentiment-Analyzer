13c5de3213518bde53b7efc3e93419a3f38a7174
Twitch_Pioneers <asimmeth@berkeley.edu>
2022-07-27 03:31:12 +0000

    testing dashboard tiles

--- src/front_end/app/home/routes.py
+++ src/front_end/app/home/routes.py
@@ -88,6 +88,7 @@ def recieve_streamer_visitors():
    elif streamer_choose_id == 'Shroud':
     return "<p> 685,765 </p>" 
 
+
 @blueprint.route('/streamer_visitors_perc')
 # @login_required
 def streamer_visitors_perc():
@@ -193,3 +194,161 @@ def get_sentiments():
 # @login_required
 def home():
    return render_template('index.html')
+
+
+### Tiles Data Pull
+
+@blueprint.route('/recieve_streamer_visitors')
+# @login_required
+def total_chat_messages():
+   streamer_choose_id = request.args.get('streamer_choose_id')
+   streamer_choose_dt = request.args.get('streamer_choose_dt')
+   print('---> test recieve_streamer_visitors: [START] --> ')
+   print(streamer_choose_id)
+   print(streamer_choose_dt)
+   print('---> test recieve_streamer_visitors: [END] --> ')
+    # set streamer name and datetime for now
+    streamer_choose_id = 'tarik'
+    streamer_choose_dt = '2022-07-26 22:57:57.779134'
+    channel_name = '\'' + channel_name + '\''
+    start_time = '\'' + start_time + '\''
+    
+    conn = sqlite3.connect('/home/w210/Twitch-chat-pioneers/src/front_end/db.sqlite3')
+    
+    total_messages_query = '''
+                        select count(*) as total_messages, 
+                        where channel_name = {} and stream_date = {}
+                        '''.format(channel_name, start_time)
+    
+    cursor_obj = conn.cursor()
+    cursor_obj.execute(total_messages_query)
+    message_count = cursor_obj.fetchall()
+    conn.commit()
+    conn.close()  
+
+    return message_count[0][0] 
+
+@blueprint.route('/recieve_streamer_visitors')
+# @login_required
+def total_chat_messages():
+   streamer_choose_id = request.args.get('streamer_choose_id')
+   streamer_choose_dt = request.args.get('streamer_choose_dt')
+   print('---> test recieve_streamer_visitors: [START] --> ')
+   print(streamer_choose_id)
+   print(streamer_choose_dt)
+   print('---> test recieve_streamer_visitors: [END] --> ')
+    # set streamer name and datetime for now
+    streamer_choose_id = 'tarik'
+    streamer_choose_dt = '2022-07-26 22:57:57.779134'
+    channel_name = '\'' + channel_name + '\''
+    start_time = '\'' + start_time + '\''
+    
+    conn = sqlite3.connect('/home/w210/Twitch-chat-pioneers/src/front_end/db.sqlite3')
+    
+    average_views_query = '''
+                        select round(avg(viewer_count)) as avg_viewers
+                        where channel_name = {} and stream_date = {}
+                        '''.format(channel_name, start_time)
+    
+    cursor_obj = conn.cursor()
+    cursor_obj.execute(average_views_query)
+    view_count = cursor_obj.fetchall()
+    conn.commit()
+    conn.close()  
+
+    return view_count[0][0] 
+
+@blueprint.route('/recieve_streamer_visitors')
+# @login_required
+def new_followers():
+   streamer_choose_id = request.args.get('streamer_choose_id')
+   streamer_choose_dt = request.args.get('streamer_choose_dt')
+   print('---> test recieve_streamer_visitors: [START] --> ')
+   print(streamer_choose_id)
+   print(streamer_choose_dt)
+   print('---> test recieve_streamer_visitors: [END] --> ')
+    # set streamer name and datetime for now
+    streamer_choose_id = 'tarik'
+    streamer_choose_dt = '2022-07-26 22:57:57.779134'
+    channel_name = '\'' + channel_name + '\''
+    start_time = '\'' + start_time + '\''
+    
+    conn = sqlite3.connect('/home/w210/Twitch-chat-pioneers/src/front_end/db.sqlite3')
+    
+    new_followers_query =  '''
+                    with starting_followers as (
+                        select follower_count
+                        from chats_table_sentiment
+                        where channel_name = {} and stream_date = {} 
+                        order by date asc
+                        limit 1)
+
+                    ,ending_followers as (
+                        select follower_count
+                        from chats_table_sentiment
+                        where channel_name = {} and stream_date = {}
+                        order by date desc
+                        limit 1
+                        )
+
+                    select * from starting_followers
+                    UNION ALL select * from ending_followers
+
+                    '''.format(channel_name, start_time, channel_name, start_time)
+    
+    cursor_obj = conn.cursor()
+    cursor_obj.execute(new_followers_query)
+    follower_count = cursor_obj.fetchall()
+    follower_change = follower_count[1][0] - follower_count[0][0]
+    conn.commit()
+    conn.close()  
+
+    return follower_change 
+
+
+@blueprint.route('/recieve_streamer_visitors')
+# @login_required
+def new_followers():
+   streamer_choose_id = request.args.get('streamer_choose_id')
+   streamer_choose_dt = request.args.get('streamer_choose_dt')
+   print('---> test recieve_streamer_visitors: [START] --> ')
+   print(streamer_choose_id)
+   print(streamer_choose_dt)
+   print('---> test recieve_streamer_visitors: [END] --> ')
+    # set streamer name and datetime for now
+    streamer_choose_id = 'tarik'
+    streamer_choose_dt = '2022-07-26 22:57:57.779134'
+    channel_name = '\'' + channel_name + '\''
+    start_time = '\'' + start_time + '\''
+    
+    conn = sqlite3.connect('/home/w210/Twitch-chat-pioneers/src/front_end/db.sqlite3')
+    
+    new_followers_query =  '''
+                    with starting_followers as (
+                        select subscriber_count
+                        from chats_table_sentiment
+                        where channel_name = {} and stream_date = {} 
+                        order by date asc
+                        limit 1)
+
+                    ,ending_followers as (
+                        select subscriber_count
+                        from chats_table_sentiment
+                        where channel_name = {} and stream_date = {}
+                        order by date desc
+                        limit 1
+                        )
+
+                    select * from starting_followers
+                    UNION ALL select * from ending_followers
+
+                    '''.format(channel_name, start_time, channel_name, start_time)
+    
+    cursor_obj = conn.cursor()
+    cursor_obj.execute(new_followers_query)
+    subscriber_count = cursor_obj.fetchall()
+    subscriber_change = subscriber_count[1][0] - subscriber_count[0][0]
+    conn.commit()
+    conn.close()  
+
+    return subscriber_change 
